1d7693c951418dd05f1b9df0ab009572b4b6bf6f
Nikcet <mstislav.kr@yandex.ru>
2025-06-23 23:09:57 +0300

    refactor

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -1,27 +1,39 @@
-import pool from '../utils/db.js';
+import pool from './db.js'
+import prisma, { waitForPrisma } from './prisma.js'
 
 export async function saveData(data: { timestamp: Date; price: number }[]) {
   for (const item of data) {
-    // Форматируем timestamp в ISO-строку с указанием временной зоны
-    const timestampISO = new Date(item.timestamp).toISOString();
+    const timestamp = new Date(item.timestamp)
+    const timestampISO = timestamp.toISOString()
 
     await pool.query(
       `INSERT INTO bitcoin_prices (timestamp, price) 
-         VALUES ($1::timestamptz, $2)
-         ON CONFLICT (timestamp) DO NOTHING`,
+       VALUES ($1::timestamptz, $2)
+       ON CONFLICT (timestamp) DO NOTHING`,
       [timestampISO, item.price]
-    );
+    )
   }
 }
 
-export async function getData(interval: string, start: Date, end: Date) {
-    return await prisma.$queryRaw`
-      SELECT 
-        date_trunc(${interval}, timestamp) as time_bucket,
-        AVG(price) as avg_price
-      FROM bitcoin_prices
-      WHERE timestamp BETWEEN ${start} AND ${end}
-      GROUP BY time_bucket
-      ORDER BY time_bucket ASC
-    `
+const allowedIntervals = ['day', 'week', 'month', 'year'] as const
+type Interval = typeof allowedIntervals[number]
+
+export async function getData(interval: Interval, start: Date, end: Date) {
+  if (!allowedIntervals.includes(interval)) {
+    throw new Error(`Invalid interval: ${interval}`)
+  }
+  await waitForPrisma();
+
+  const query = `
+    SELECT 
+      date_trunc('${interval}', timestamp) as time_bucket,
+      AVG(price) as avg_price
+    FROM bitcoin_prices
+    WHERE timestamp BETWEEN $1 AND $2
+    GROUP BY time_bucket
+    ORDER BY time_bucket ASC
+  `
+
+  const result = await prisma.$queryRawUnsafe(query, start, end)
+  return result
 }
